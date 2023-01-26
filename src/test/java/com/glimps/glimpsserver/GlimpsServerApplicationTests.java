34aa28cf6ff7ce5f10f9ba11786b66617c8a3dd6
mikekang47 <gyeong0308@gmail.com>
2023-01-26 22:54:05 +0900

    Init project

---
+++ src/test/java/com/glimps/glimpsserver/GlimpsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glimps.glimpsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlimpsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
