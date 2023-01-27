63a36d4567d313daf14889bcaf6859e0ec22570f
mikekang47 <gyeong0308@gmail.com>
2023-01-27 23:21:07 +0900

    Feat: create SecurityConfig

---
+++ src/main/java/com/glimps/glimpsserver/common/config/SpringSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.glimps.glimpsserver.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		//TODO: add security filter chain
+		return httpSecurity
+			.csrf().disable()
+			.build();
+	}
+}
