6f5f360ecb8846d3c5bcfa3b0574283012d610c2
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-01-28 14:53:08 +0900

    Feat: entities related to perfume added[Perfume, Note, PerfumeNotes, Hashtag, PerfumeTags, PerfumePhoto]

--- src/main/java/com/glimps/glimpsserver/review/domain/HeartReview.java
+++ src/main/java/com/glimps/glimpsserver/review/domain/HeartReview.java
@@ -1,11 +1,6 @@
 package com.glimps.glimpsserver.review.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import com.glimps.glimpsserver.user.domain.User;
 
@@ -14,17 +9,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "heart_reviews")
+@Entity
 public class HeartReview {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
 	private User user;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "review_id")
 	private Review review;
 }
