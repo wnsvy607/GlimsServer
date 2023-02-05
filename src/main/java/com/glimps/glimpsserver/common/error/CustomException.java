976bd0dc4dccdbd207952c594c5519fef83fdbe3
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-02-06 02:33:46 +0900

    Feat: CustomException Constructor logic modified

--- src/main/java/com/glimps/glimpsserver/common/error/CustomException.java
+++ src/main/java/com/glimps/glimpsserver/common/error/CustomException.java
@@ -5,11 +5,10 @@ import lombok.Getter;
 @Getter
 public class CustomException extends RuntimeException {
 	private final ErrorCode errorCode;
-	private final String message;
 
-	public CustomException(ErrorCode errorCode, String message) {
-		super(message);
-		this.message = message;
+	public CustomException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
 		this.errorCode = errorCode;
 	}
+	
 }
