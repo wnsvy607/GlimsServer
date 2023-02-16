8bbbe292a20c5d124ee6b8e45e6e485b58ba5557
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-02-16 13:21:42 +0900

    Feat: UserRepository added

--- src/main/java/com/glimps/glimpsserver/user/infra/UserRepository.java
+++ src/main/java/com/glimps/glimpsserver/user/infra/UserRepository.java
@@ -1,6 +1,7 @@
 package com.glimps.glimpsserver.user.infra;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +10,8 @@ import com.glimps.glimpsserver.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
 	@Query("select u from User u where u.email = ?1")
 	List<User> findAllByEmail(String email);
+
+	Optional<User> findByEmail(String email);
 }
