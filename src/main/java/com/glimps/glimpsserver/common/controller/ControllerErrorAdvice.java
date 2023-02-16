89ffa92d4a9b14327fbde1d45f8eda3eefa12d40
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-02-16 16:50:13 +0900

    Feat: MemberDuplicationException handling

--- src/main/java/com/glimps/glimpsserver/common/controller/ControllerErrorAdvice.java
+++ src/main/java/com/glimps/glimpsserver/common/controller/ControllerErrorAdvice.java
@@ -10,6 +10,7 @@ import com.glimps.glimpsserver.common.dto.ErrorResponse;
 import com.glimps.glimpsserver.common.error.CustomException;
 import com.glimps.glimpsserver.common.error.EntityNotFoundException;
 import com.glimps.glimpsserver.common.error.InvalidTokenException;
+import com.glimps.glimpsserver.common.error.MemberDuplicationException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -63,5 +64,17 @@ public class ControllerErrorAdvice {
 		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode().getStatus(),
 			e.getMessage() + " Token: " + e.getToken());
 		return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+	}
+
+	/**
+	 * 회원가입시 같은 이메일을 가진 유저가 이미 있을 때 어디로 가입되어 있는지 반환
+	 */
+	@ExceptionHandler(MemberDuplicationException.class)
+	protected ResponseEntity<ErrorResponse> handleDuplicateRequestException(MemberDuplicationException e) {
+		log.error("Exception occurs: {}, User type: {}", e.getMessage(), e.getUserType());
+		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode().getStatus(),
+			e.getMessage() + " UserType: " + e.getUserType());
+		return ResponseEntity.status(e.getErrorCode().getStatus())
+			.body(errorResponse);
 	}
 }
