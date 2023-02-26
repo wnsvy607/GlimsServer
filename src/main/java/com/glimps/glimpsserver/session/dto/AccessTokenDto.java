d164ffa733ce922dba4346a85789b0d6fc41c4f2
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-02-27 01:11:07 +0900

    Docs: add Swagger description annotations to Dtos & Controllers

--- src/main/java/com/glimps/glimpsserver/session/dto/AccessTokenDto.java
+++ src/main/java/com/glimps/glimpsserver/session/dto/AccessTokenDto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +14,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class AccessTokenDto {
 
+	@Schema(description = "grantType", example = "Bearer", required = true)
 	private String grantType;
+	@Schema(description = "access Token", example = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1MiLCJpYXQiOjE2NjM5MTczNjQsImV4cCI6MTY2MzkxODI2NCwibWVtYmVySWQiOjEsInJvbGUiOiJBRE1JTiJ9.WVqT2UR9qSh-CqoBbq0xkUOKRYOFIwpWKeDCKMzlXXwXFie7Zt8laaDij6X2R4KxTtDNSoLoMbFkYB4h-M-wHQ", required = true)
 	private String accessToken;
+	@Schema(description = "access Token 만료 시간", example = "2022-09-23 16:31:04", required = true)
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS", timezone = "Asia/Seoul")
 	private Date accessTokenExpireTime;
 }
--- src/main/java/com/glimps/glimpsserver/session/presentation/LogOutController.java
+++ src/main/java/com/glimps/glimpsserver/session/presentation/LogOutController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.glimps.glimpsserver.common.authentication.UserAuthentication;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "authentication", description = "로그아웃/토큰재발급 API")
 @RestController
 @RequestMapping("/api/v1")
 public class LogOutController {
 
-
+	@Tag(name = "authentication")
+	@Operation(summary = "로그아웃 API", description = "로그아웃시 서버 DB 내부 refresh token 만료 처리")
 	@PostMapping("/logout")
 	public Map<String, String> logout(UserAuthentication userAuthentication) {
 
