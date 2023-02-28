621c1483f19922b2965e850d82488bc810e206de
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-03-01 00:54:32 +0900

    Test: add @WithMockCustomUser annotation & factory to make test easier

--- src/test/java/com/glimps/glimpsserver/common/presentation/TestControllerTest.java
+++ src/test/java/com/glimps/glimpsserver/common/presentation/TestControllerTest.java
@@ -1,41 +1,43 @@
 package com.glimps.glimpsserver.common.presentation;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.glimps.glimpsserver.common.config.SecurityConfig;
-import com.glimps.glimpsserver.common.filter.JwtAuthenticationFilter;
 import com.glimps.glimpsserver.testconfig.WithMockCustomUser;
+import com.glimps.glimpsserver.user.domain.RoleType;
 
-@WebMvcTest(controllers = HealthCheckController.class,
+@WebMvcTest(controllers = TestController.class,
 	excludeFilters = {
 		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SecurityConfig.class})
 	})
-// @ImportAutoConfiguration(exclude = {OAuth2ClientAutoConfiguration.class, OAuth2ResourceServerAutoConfiguration.class})
-class HealthCheckControllerTest {
-
+class TestControllerTest {
+	private static final String EMAIL = "user12345@naver.com";
+	private static final String ROLE = "ROLE_USER";
 	@Autowired
 	private MockMvc mockMvc;
 
-	@WithMockCustomUser
+	@WithMockCustomUser(userName = EMAIL, role = ROLE)
 	@Test
-	void HealthCheckTest() throws Exception {
+	void AuthenticationTest() throws Exception {
 
-		mockMvc.perform(get("/health"))
-			.andExpect(MockMvcResultMatchers.status().isOk());
+		mockMvc.perform(get("/test")).andExpect(status().isOk())
+			.andExpect(status().isOk())
+			.andExpect(content().string(EMAIL + "has been authenticated."));
 
 	}
+
 
 }
